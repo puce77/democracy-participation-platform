50fc3cae0ec8d32f533229781cab25e2ac5fd273
Florian Brunner <fbrunnerreg@gmx.ch>
2021-02-06 18:05:24 +0100

    initial import

---
+++ democracyparticipationplatform-vaadin/src/main/java/com/github/puce77/democracyparticipationplatform/DemocracyParticipationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.puce77.democracyparticipationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocracyParticipationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocracyParticipationPlatformApplication.class, args);
+	}
+
+}
